33d5251bacbe618e0ae363014e5f8f5d226edc9f
Gozi1 <snwabiani@gmail.com>
2023-01-04 23:11:48 +0000

    nodemon up

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-
+app.get("/",(req,res) => {
+  res.redirect("view/urls_index");
+})
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
