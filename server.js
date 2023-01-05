54143bae83a99cd3d5e3d0b272d817b7aec87d34
Gozi1 <snwabiani@gmail.com>
2023-01-05 18:43:18 +0000

    install sass package

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const sassMiddleware = require('./lib/sass-middleware');
+
+
+
 const PORT = 8080;
 
 const app = express();
 
+
 app.set("view engine", "ejs");
+
+app.use(
+  '/styles',
+  sassMiddleware({
+    source: __dirname + '/styles',
+    destination: __dirname + '/public/styles',
+    isSass: false, // false => scss, true => sass
+  })
+);
 
 app.get("/",(req,res) => {
   res.render('urls_index');
